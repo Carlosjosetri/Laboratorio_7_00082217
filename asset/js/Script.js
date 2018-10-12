82643bfd8bf8b3eb87fc5bf627d81bdf476ff07a
Carlosjosetri <00082217@uca.edu.sv>
2018-10-12 02:01:42 -0600

    initial commit

---
+++ asset/js/Script.js
@@ -0,0 +1,20 @@
+var container = document.getElementById("c1");
+
+container.innerText="¡Hola Mundo!";
+container.style.backgroundColor= "#000000";
+container.style.color="#1cb723";
+container.style.width="200px";
+container.style.height="200px";
+container.innerHTML= "<input type='text' placeholder='escriba su texto'></input>";
+
+
+var containers=document.querySelectorAll(".c2");
+
+
+containers.forEach(function(element) {
+ element.style.backgroundColor="#009999";    
+});
+
+containers.forEach(function(element) {
+    element.innerHTML= "<input type='text' placeholder='escriba su texto'></input>";
+   });
